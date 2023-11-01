a87dfd2df047057609c16dffbf7d42de097942be
maximiliangertz <maximilian.gertz@accenture.com>
2023-11-01 12:53:10 +0100

    Adjusted conf and statusbar

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 cd $1
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
@@ -6,6 +6,6 @@ if test "$BRANCH" != ""; then
   if test "$STATUS" = "0"; then
     echo "#[fg=green,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
   else
-    echo "#[fg=red,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=red,bold]   $BRANCH  "
+    echo "#[fg=black,bg=#F7778F,bold]   $BRANCH  "
   fi
 fi
