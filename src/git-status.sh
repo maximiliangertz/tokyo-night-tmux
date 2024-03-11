7127c897f49ca89d56485cde05b2493e63d708ae
maximiliangertz <maximilian.gertz@accenture.com>
2024-03-11 11:57:01 +0100

    Adjusted colors :)

--- src/git-status.sh
+++ src/git-status.sh
@@ -4,8 +4,8 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
 if test "$BRANCH" != ""; then
   if test "$STATUS" = "0"; then
-    echo "#[fg=green,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
+    echo "#[fg=green,bg=#9BE206,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
   else
-    echo "#[fg=black,bg=#F7778F,bold]   $BRANCH  "
+    echo "#[fg=black,bg=#9BE206,bold]   $BRANCH  "
   fi
 fi
